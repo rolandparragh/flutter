222566ade046d953edfe277fd53a4fefc212bc6e
Roland Parragh <roland.parragh@sap.com>
2024-12-26 23:03:50 +0100

    Added counter and stuff

--- helloword/lib/main.dart
+++ helloword/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   bool isProgressing = false;
   double progress = 0.0;
+  int count = 0;
 
   @override
   void initState() {
@@ -28,6 +29,7 @@ class _MyAppState extends State<MyApp> {
           startProgres();
         } else if (progress >= 1.0) {
           setState(() {
+            count += 1;
             progress = 0.0;
           });
 
@@ -44,36 +46,56 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(title: Text('Loading Bar')),
         body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              FractionallySizedBox(
-                widthFactor: 0.8,
-                child: LinearProgressIndicator(
-                  value: progress,
-                  backgroundColor: Colors.grey[300],
-                  color: Colors.blue,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FractionallySizedBox(
+              widthFactor: 0.8,
+              child: LinearProgressIndicator(
+                value: progress,
+                backgroundColor: Colors.grey[300],
+                color: Colors.blue,
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  "Start Progress",
                 ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Start Progress"),
-                  Switch(
-                      value: isProgressing,
-                      onChanged: (bool value) {
-                        setState(() {
-                          isProgressing = value;
-                          if (isProgressing) {
-                            progress = 0.0;
-                            startProgres();
-                          }
-                        });
-                      })
-                ],
-              ),
-            ]),
+                Switch(
+                    value: isProgressing,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isProgressing = value;
+                        if (isProgressing) {
+                          progress = 0.0;
+                          startProgres();
+                        }
+                      });
+                    }),
+              ],
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text("Count: ${count.toString()}"),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      count = 0;
+                      progress = 0.0;
+                      isProgressing = false;
+                    });
+                  },
+                  child: Text('Reset the counter'),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
